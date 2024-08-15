d1ca40e63a3fe29fdf4b062ed72bfeee5ae075ea
ckritzinger <ckritzinger@gmail.com>
2024-08-15 20:26:08 +0200

    disable luno and binance for geewiz

--- src/components/WalletSelect.vue
+++ src/components/WalletSelect.vue
@@ -9,11 +9,20 @@ export default {
   },
   computed: {
     ...mapStores(usePaymentStore),
+    lunoDisabled() {
+      return !this.paymentsStore.enabledWallets.includes('luno')
+    },
+    binanceDisabled() {
+      return !this.paymentsStore.enabledWallets.includes('binance')
+    },
+    valrDisabled() {
+      return !this.paymentsStore.enabledWallets.includes('valr')
+    },
   },
   methods: {
     setWallet(wallet: Wallet) {
       this.paymentsStore.setWallet(wallet)
-    }
+    },
   },
   data() {
     return {
@@ -29,25 +38,28 @@ export default {
         <p>Choose the wallet you want to pay with</p>
         <div>
             <ul>
-              <li>
+                <li>
                    <button class="choose-wallet-btn lightning my-3 py-2 rounded w-[300px]" @click="setWallet(Wallet.wallets['lightning'])">
                       <img src="@/assets/wallets/lightning.png" class="object-contain w-full h-full"></img>
                    </button>
                 </li>
                 <li>
-                   <button class="choose-wallet-btn binance my-3 py-3 px-4 rounded w-[300px]" @click="setWallet(Wallet.wallets['binance'])">
-                      <img src="@/assets/wallets/binance.png" class="object-contain w-full h-full"></img>
-                   </button>
-                </li>
-                <li>
-                   <button class="choose-wallet-btn luno my-3 py-2 rounded w-[300px]" @click="setWallet(Wallet.wallets['luno'])">
-                      <img src="@/assets/wallets/luno.png" class="object-contain w-full h-full"></img>
-                   </button>
-                </li>
-                <li>
-                   <button class="choose-wallet-btn valr my-3 py-3 rounded w-[300px]" @click="setWallet(Wallet.wallets['valr'])">
+                   <button class="choose-wallet-btn valr my-3 py-3 rounded w-[300px]" @click="setWallet(Wallet.wallets['valr'])" :disabled="valrDisabled">
                       <img src="@/assets/wallets/valr.png" class="object-contain w-full h-full"></img>
                    </button>
+                   <div v-if="valrDisabled" class="overlay">Coming Soon</div>
+                </li>
+                <li>
+                   <button class="choose-wallet-btn binance my-3 py-3 px-4 rounded w-[300px]" @click="setWallet(Wallet.wallets['binance'])" :disabled="binanceDisabled">
+                      <img src="@/assets/wallets/binance.png" class="object-contain w-full h-full"></img>
+                   </button>
+                   <div v-if="binanceDisabled" class="overlay">Coming Soon</div>
+                </li>
+                <li>
+                   <button class="choose-wallet-btn luno my-3 py-2 rounded w-[300px]" @click="setWallet(Wallet.wallets['luno'])" :disabled="lunoDisabled">
+                      <img src="@/assets/wallets/luno.png" class="object-contain w-full h-full"></img>
+                   </button>
+                   <div v-if="lunoDisabled" class="overlay">Coming Soon</div>
                 </li>
             </ul>
         </div>
@@ -78,16 +90,49 @@ export default {
     border: 2px solid #3645f1;
   }
 }
+
 .binance {
   background-color: #eeb80d;
   &:hover {
     border: 2px solid #fadc7a;
   }
 }
+
 .lightning {
   background-color: #4a4a4a;
   &:hover {
     border: 2px solid #6b6b6b;
   }
+}
+
+li {
+  position: relative;
+}
+
+button:disabled {
+  opacity: 0.25;
+  &:hover {
+    transform: none;
+  }
+  &:hover{
+    border: none;
+  }
+}
+
+
+.overlay {
+    opacity: 1;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    color: #fafafa;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 12px;
+    text-align: center;
+    pointer-events: none; /* Allows clicks to pass through the overlay */
 }
 </style>
--- src/stores/payments.ts
+++ src/stores/payments.ts
@@ -13,6 +13,7 @@ export const usePaymentStore = defineStore('payments', {
     invoice: {} as Invoice,
     errors: [] as string[],
     status: PaymentStatus.Loading,
+    enabledWallets: ['lightning', 'valr'] as string[],
   }),
   getters: {
     paidAt: (state): string => state.invoice.paid_at || '',
@@ -33,6 +34,11 @@ export const usePaymentStore = defineStore('payments', {
       if (this.invoiceParams.errors.length > 0) {
         this.errors = this.invoiceParams.errors
         this.status = PaymentStatus.Error
+      }
+      // TODO: replace this with server-side configuration (get from API)
+      if(this.invoiceParams.merchantCode !== 'geewiz'){
+        this.enabledWallets.push('binance')
+        this.enabledWallets.push('luno')
       }
     },
     async setWallet(wallet: Wallet) {

f7990864aae31c5f9f3585acb1c5ede3aab5abec
Carl Kritzinger <ckritzinger@gmail.com>
2025-08-12 08:37:03 +0200

    basic hotjar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Hotjar from '@hotjar/browser';
+const siteId = 6489570;
+const hotjarVersion = 6;
+Hotjar.init(siteId, hotjarVersion);
 </script>
 
 <template>

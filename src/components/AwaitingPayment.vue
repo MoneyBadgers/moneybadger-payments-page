993fb8a78d7c28efcfae4ef6f62bb0bc82d3a1ae
Carl Kritzinger <ckritzinger@gmail.com>
2025-08-14 22:15:07 +0200

    twiddle UI on awaiting payment page

--- src/components/AwaitingPayment.vue
+++ src/components/AwaitingPayment.vue
@@ -12,6 +12,13 @@ import { usePaymentStore } from '../stores/payments'
 import { AnalyticsEvent } from '../types/analytics_events'
 import { defaultAnalyticproperties } from '../types/analytics_default_properties'
 
+function isMobileDevice() {
+  return /Android|iPhone|iPad|iPod/i.test(navigator.userAgent)
+}
+function isDesktopDevice() {
+  return !isMobileDevice()
+}
+
 export default {
   name: 'AwaitingPayment',
   emits: ['change-wallet'],
@@ -23,10 +30,11 @@ export default {
   },
   props: {
     invoice: { type: Object as PropType<Invoice>, required: true },
-    wallet: { type: Object as PropType<Wallet>, required: true},
+    wallet: { type: Object as PropType<Wallet>, required: true }
   },
   data() {
     return {
+      showQr: isDesktopDevice(), // Detect mobile devices
       showCopyHint: false,
       clipboard: null as any,
       qrLoading: true,
@@ -40,24 +48,13 @@ export default {
   computed: {
     ...mapStores(usePaymentStore),
     isMobileDevice() {
-      return /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+      return true // ;
     },
     isDesktopDevice() {
       return !this.isMobileDevice
     },
     paymentRequest(): string {
       return this.invoice.payment_request?.data || ''
-    },
-    showQr(): boolean {
-      if (this.isDesktopDevice) {
-        return true
-      }
-      if (this.forceShowQr) {
-        return true
-      }
-
-      // there is no logic for the button as it is always visible
-      return false
     },
     paymentRequestQrUrl(): string | null {
       // Only return custom QR code URL for Luno and Binance invoices
@@ -70,59 +67,56 @@ export default {
       return null
     },
     paymentRequestQrData(): string {
-      if(this.invoice.payment_request?.qr_code_content) {
+      if (this.invoice.payment_request?.qr_code_content) {
         return this.invoice.payment_request.qr_code_content
       }
       // if this is VALR and we have defined a payment currency to use,
       // then select the payment data for that currency
-      if(this.wallet.valueStore === 'valr' && this.paymentsStore.getPaymentCurrency){
-          let key = `valr|${this.paymentsStore.getPaymentCurrency}`
-          let pm = this.invoice.payment_request?.payment_methods
-          if(pm && pm[key]){
-            return this.wallet.generateCopyableRequest(pm[key] as unknown as string)
-          }
+      if (this.wallet.valueStore === 'valr' && this.paymentsStore.getPaymentCurrency) {
+        let key = `valr|${this.paymentsStore.getPaymentCurrency}`
+        let pm = this.invoice.payment_request?.payment_methods
+        if (pm && pm[key]) {
+          return this.wallet.generateCopyableRequest(pm[key] as unknown as string)
+        }
       }
       return this.wallet.generateCopyableRequest(this.paymentRequest)
     },
     paymentRequestDeepLink(): string {
-      if(this.invoice.payment_request?.deeplink) {
+      if (this.invoice.payment_request?.deeplink) {
         return this.invoice.payment_request.deeplink
       }
       // if this is VALR and we have defined a payment currency to use,
       // then select the payment data for that currency
-      if(this.wallet.valueStore === 'valr' && this.paymentsStore.getPaymentCurrency){
+      if (this.wallet.valueStore === 'valr' && this.paymentsStore.getPaymentCurrency) {
         let key = `valr|${this.paymentsStore.getPaymentCurrency}`
-          let pm = this.invoice.payment_request?.payment_methods
-          if(pm && pm[key]){
-            let link = this.wallet.generateLink(pm[key] as unknown as string)
-            // This is a workaround for VALR's deeplink format
-            // which currently does not support /en in the URL
-            let valrFixedLink = link.replace(/(valr\.com)\/en(?=\/payments)/, '$1');
-            return valrFixedLink
-          }
+        let pm = this.invoice.payment_request?.payment_methods
+        if (pm && pm[key]) {
+          let link = this.wallet.generateLink(pm[key] as unknown as string)
+          // This is a workaround for VALR's deeplink format
+          // which currently does not support /en in the URL
+          let valrFixedLink = link.replace(/(valr\.com)\/en(?=\/payments)/, '$1')
+          return valrFixedLink
+        }
       }
       return this.wallet.generateLink(this.paymentRequest)
     },
     customProtocolDeeplink(): string {
-      if(this.wallet.valueStore === 'valr' && this.isMobileDevice) {
+      if (this.wallet.valueStore === 'valr' && this.isMobileDevice) {
         // VALR uses a custom protocol for deep linking
         return this.paymentRequestDeepLink.replace('https://', 'valr://')
       }
-      return ""
+      return ''
     },
     currentDeeplink(): string {
-      let deeplinks = [
-        this.customProtocolDeeplink,
-        this.paymentRequestDeepLink
-      ]
+      let deeplinks = [this.customProtocolDeeplink, this.paymentRequestDeepLink]
       // remove empty deeplinks
-      deeplinks = deeplinks.filter(link => link.length > 0)
+      deeplinks = deeplinks.filter((link) => link.length > 0)
       // index by walletOpens to cycle through deeplinks
       return deeplinks[this.walletOpens % deeplinks.length] || ''
     },
     expiresIn(): string {
       // Use currentTime to force updates
-      const expires = new Date(this.invoice.expires_at ?? 0);
+      const expires = new Date(this.invoice.expires_at ?? 0)
       return formatDistanceStrict(expires, this.currentTime, { addSuffix: true })
     }
   },
@@ -136,10 +130,10 @@ export default {
     })
 
     this.trackAnalytics(AnalyticsEvent.WaitingForPayment, {
-        'isDesktopDevice': this.isDesktopDevice,
-        'qrVisible': this.showQr,
-        'forceShowQr': this.forceShowQr,
-      })
+      isDesktopDevice: this.isDesktopDevice,
+      qrVisible: this.showQr,
+      forceShowQr: this.forceShowQr
+    })
 
     // Add this interval
     this.timer = setInterval(() => {
@@ -158,17 +152,21 @@ export default {
   methods: {
     copyPaymentRequest() {
       // not really sure what makes sense to copy for Binance
-      const content = this.wallet.valueStore == 'binance' ?
-        this.paymentRequestDeepLink :
-        this.paymentRequestQrData
+      const content =
+        this.wallet.valueStore == 'binance'
+          ? this.paymentRequestDeepLink
+          : this.paymentRequestQrData
       this.trackAnalytics(AnalyticsEvent.CopyPaymentRequest, {
-        'content': content,
+        content: content
       })
-      navigator.clipboard.writeText(content).then(() => {
-        this.flashCopyHint()
-      }).catch(err => {
-        console.error('Failed to copy: ', err)
-      })
+      navigator.clipboard
+        .writeText(content)
+        .then(() => {
+          this.flashCopyHint()
+        })
+        .catch((err) => {
+          console.error('Failed to copy: ', err)
+        })
     },
     flashCopyHint() {
       this.showCopyHint = true
@@ -184,25 +182,32 @@ export default {
     },
     onOpenWallet() {
       this.trackAnalytics(AnalyticsEvent.OpenWalletButtonClicked, {
-        'walletOpens': this.walletOpens,
-        'deeplink': this.currentDeeplink,
+        walletOpens: this.walletOpens,
+        deeplink: this.currentDeeplink
       })
       // could hook in peach payments postMessage here
       setTimeout(() => {
-              this.walletOpens = this.walletOpens + 1
+        this.walletOpens = this.walletOpens + 1
       }, 100)
     },
     trackAnalytics(event: AnalyticsEvent, additionalProps?: Record<string, any>) {
-      this.$mixpanel.trackEvent(event, {...defaultAnalyticproperties({
-        wallet: this.$props.wallet.name,
-        currency: this.$props.invoice.currency,
-        merchant: this.$props.invoice.merchant_name,
-        amount: this.$props.invoice.amount_cents,
-      }), ...additionalProps})
+      this.$mixpanel.trackEvent(event, {
+        ...defaultAnalyticproperties({
+          wallet: this.$props.wallet.name,
+          currency: this.$props.invoice.currency,
+          merchant: this.$props.invoice.merchant_name,
+          amount: this.$props.invoice.amount_cents
+        }),
+        ...additionalProps
+      })
     },
     forceShowQrCode() {
       this.forceShowQr = true
       this.trackAnalytics(AnalyticsEvent.ShowQrCodeButtonClicked)
+    },
+    forceShowOpenWallet() {
+      this.forceShowQr = false
+      this.trackAnalytics(AnalyticsEvent.ShowOpenWalletButtonClicked)
     },
     changeWallet() {
       this.trackAnalytics(AnalyticsEvent.ChangeWallet)
@@ -215,6 +220,16 @@ export default {
 <template>
   <div>
     <div @click="copyPaymentRequest" class="justify-center">
+      <h5 class="text-gray-200 font-bold py-1 text-sm" @click="copyPaymentRequest">
+        <div v-if="!showCopyHint" class="flex justify-center mx-4 w-300">
+          <span class="tap-to-copy">Tap to copy {{ wallet.invoiceType }}</span>
+          <ClipboardDocumentIcon class="mx-2 size-6 text-yellow-500" />
+        </div>
+        <div v-if="showCopyHint" class="flex justify-center mx-4 w-300">
+          <span>Payment details copied to clipboard!</span>
+          <ClipboardDocumentCheckIcon class="mx-2 size-6 text-yellow-500" />
+        </div>
+      </h5>
       <div v-if="showQr" class="w-full flex flex-col justify-center items-center">
         <h4 class="text-gray-200 font-bold mb-2">Scan QR code with {{ wallet.scanner }}</h4>
         <div v-if="paymentRequestQrUrl && !qrLoadError" class="center">
@@ -232,52 +247,37 @@ export default {
           <qrcode-vue :value="paymentRequestQrData" :size="300" :margin="3" level="L" />
         </div>
       </div>
-      <div v-else class="w-full">
-        <h4 class="text-gray-200 font-bold mb-2">Use your {{ wallet.scanner }} to pay</h4>
-      </div>
-      <h5 class="text-gray-200 font-bold py-1 text-sm" @click="copyPaymentRequest">
-        <div v-if="!showCopyHint" class="flex justify-center mx-4 w-300">
-          <span class="tap-to-copy">Or tap to copy {{ wallet.invoiceType }}</span>
-          <ClipboardDocumentIcon class="mx-2 size-6 text-yellow-500" />
-        </div>
-        <div v-if="showCopyHint" class="flex justify-center mx-4 w-300">
-          <span>Payment details copied to clipboard!</span>
-          <ClipboardDocumentCheckIcon class="mx-2 size-6 text-yellow-500" />
-        </div>
-      </h5>
+    </div>
+    <div v-if="!showQr" class="w-full">
       <div class="w-full flex flex-col justify-center items-center mt-4">
         <a
           :href="currentDeeplink"
           target="_blank"
           @click="onOpenWallet"
-          class="open-wallet-btn py-2 rounded w-[300px]"
+          class="open-wallet-btn w-[300px] flex flex-col items-center justify-center text-center rounded-lg shadow-lg py-8"
           :class="{ 'md:hidden': wallet.valueStore == 'binance' }"
         >
-          Open {{ wallet.scanner }}
+          <div class="text-xl font-bold text-black">Click here to pay</div>
+          <div class="text-sm text-black">the {{ wallet.scanner }} will open</div>
         </a>
       </div>
     </div>
-    <button
-      @click="changeWallet"
-      class="change-wallet-btn py-2 mt-5 rounded w-[300px]"
-    >
-      Change Wallet
-    </button>
-    <a v-if="!showQr"
-      @click="forceShowQrCode"
-      class="w-full mt-4 ml-1 underline hover:text-indigo-200 transition-colors"
-    >
-      Scan a QR code instead
-    </a>
-    <p class="mt-3">This Payment Expires {{ expiresIn }}</p>
   </div>
-  <p style="display: none;">
+  <p class="mt-3">This Payment Expires {{ expiresIn }}</p>
+  <button @click="changeWallet" class="change-wallet-btn py-2 mt-5 rounded w-[300px]">
+    Change Wallet
+  </button>
+  <a @click="showQr = !showQr" class="text-center mt-3">
+    <span v-if="!showQr">Show QR code</span>
+    <span v-else>I'm on mobile</span>
+  </a>
+  <div style="display: none">
     <ul>
       <li>currentDeeplink: {{ currentDeeplink }}</li>
       <li>customProtocolDeeplink: {{ customProtocolDeeplink }}</li>
       <li>paymentRequestDeepLink: {{ paymentRequestDeepLink }}</li>
     </ul>
-  </p>
+  </div>
 </template>
 
 <style scoped>
@@ -308,6 +308,7 @@ h4 {
   color: var(--color-black);
   text-decoration: none;
   text-align: center;
+  box-shadow: 0px 0px 2px 1px yellow;
   &:hover {
     background-color: var(--color-amber-light);
     color: var(--color-black);
